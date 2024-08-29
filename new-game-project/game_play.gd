ae3b2d289c45a2bd3dc9cbbf540e635e6e39721b
Juanmileshome <mileshaus@gmail.com>
2024-08-29 12:58:26 -0400

    Add files via upload

---
+++ new-game-project/game_play.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if (GlobalVars.getCheck()) :
+		print(GlobalVars.getCheck())
+		GlobalVars.setCheck(false)
+		get_tree().reload_current_scene()

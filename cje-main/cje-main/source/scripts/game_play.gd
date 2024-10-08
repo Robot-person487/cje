ffb3f9080b57a5839b013be86fa72d971bc25970
8bitArcher <kxskeete@gmail.com>
2024-10-08 11:40:49 -0400

    Game (Health Bar + Level Up Update)
    - Added health bar
    - Secrets (if they weren't there before)
    - Moves on to next level

---
+++ cje-main/cje-main/source/scripts/game_play.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+var slimepath = preload("res://source/scenes/slime.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if (GlobalVars.getDeath()) :
+		print(GlobalVars.getDeath())
+		GlobalVars.setDeath(false)
+		get_tree().reload_current_scene()
+
+
+func _on_slime_bounce_entered():
+	$player.velocity.y += -600
+	$player.bounceBuildUp += 100
+
+
+func _on_Death_entered(body):
+	if (body.is_in_group("Death")) :
+		GlobalVars.setDeath(true)
+
+
+func _on_slime_2_bounce_entered() -> void:
+	$player.velocity.y = -600
+	$player.bounceBuildUp += 50
+
+
+func _on_level_complete_body_entered(body: Node2D) -> void:
+	get_tree().change_scene_to_file("res://source/scenes/game_play.tscn")
